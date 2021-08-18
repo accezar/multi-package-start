b1fd2fba35412b397b2ee3424b43533d427311de
Anna Carolina <carol.carizzo@gmail.com>
2021-08-18 03:20:39 -0300

    chore(storybook): add storybook

---
+++ packages/pkg-1/src/Pkg1.component.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Pkg1 = () => <div>Pkg1 div</div>;
+
+export default Pkg1;
--- packages/pkg-1/src/pkg-1.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = pkg1;
-
-function pkg1() {
-  // TODO
-  console.log('hi from pkg 1');
-}
